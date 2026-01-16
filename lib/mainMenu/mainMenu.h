c0fdda8a9bd33ce099d1155308ba16b83b29842b
Aditya.D.S <aditya.devendran.s@gmail.com>
2026-01-16 07:32:29 -0500

    Added live time update

--- lib/mainMenu/mainMenu.h
+++ lib/mainMenu/mainMenu.h
@@ -1,4 +1,6 @@
 #include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/posix/time.h>
+#include <time.h>
 
 //1f55d926-12bb-11ee-be56-0242ac120007
 #define MAINMENU_SERVICE_UUID_VAL BT_UUID_128_ENCODE(0x1f55d926, 0x12bb, 0x11ee, 0xbe56, 0x0242ac120007)
@@ -21,7 +23,14 @@ static ssize_t timeSet(struct bt_conn *conn, const struct bt_gatt_attr *attr,
 
     unixTime = *(uint64_t *)buf;
     printk("Received time: %llu\n", unixTime);
+    struct timespec ts;
+    ts.tv_sec = unixTime;
+    ts.tv_nsec = 0;
 
+    if (clock_settime(CLOCK_REALTIME, &ts) != 0) {
+        printk("Failed to set system time\n");
+        return -EIO;
+    }
     return len;
 }
 
@@ -36,7 +45,54 @@ BT_GATT_SERVICE_DEFINE(mainmenu_service,
     //BT_GATT_CCC(my_ccc_cfg_changed, BT_GATT_PERM_READ | BT_GATT_PERM_WRITE),
 );
 
-void MainMenu()
+K_SEM_DEFINE(timeUpdate_sem, 0, 1);
+
+void timeUpdate()
+{
+    struct timespec ts;
+    if (clock_gettime(CLOCK_REALTIME, &ts) != 0) {
+        printk("Failed to get system time\n");
+        return;
+    }
+
+    printk("Current time: %lld\n", (long long)ts.tv_sec);
+
+    unixTime = ts.tv_sec;
+    k_sem_give(&timeUpdate_sem);
+}
+
+void MainMenu(void)
 {
     printk("Main Menu Loaded\n");
+    struct k_timer timer;
+    k_timer_init(&timer, timeUpdate, NULL);
+    k_timer_start(&timer, K_SECONDS(1), K_SECONDS(1));
+
+    lv_style_t style;
+    lv_style_init(&style);
+        
+    lv_style_set_text_font(&style, &TallerFont);
+    lv_obj_t *time_label = lv_label_create(lv_scr_act());
+    lv_obj_add_style(time_label, &style, 0); 
+        
+    while(1)
+    {
+        //render
+        
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+
+        struct tm time_info;
+        char buffer[6];
+        gmtime_r(&(ts.tv_sec), &time_info);
+
+        strftime(buffer, sizeof(buffer), "%H:%M", &time_info);
+        lv_label_set_text(time_label, buffer);
+        
+        lv_task_handler();
+        //wait
+        k_sem_take(&timeUpdate_sem, K_FOREVER);
+    }
 }
+
+K_THREAD_DEFINE(mainMenu_thread, 16384, MainMenu, NULL, NULL, NULL, 7, 0, 7000);
--- src/main.c
+++ src/main.c
@@ -16,7 +16,6 @@ LV_FONT_DECLARE(TallerFont);
 
 int main(void)
 {
-        k_sleep(K_SECONDS(3));
         printk("LVGL Test Application Start\n");
         
         //bluetoothActivate();
@@ -40,33 +39,8 @@ int main(void)
                 printk("Failed to set required pixel format\n");
         }
 
-        lv_style_t style;
-        lv_style_init(&style);
-        
-        lv_style_set_text_font(&style, &TallerFont);
-        lv_obj_t *hello_label = lv_label_create(lv_scr_act());
-        lv_label_set_text(hello_label, "12:30");
-        lv_obj_add_style(hello_label, &style, 0);
-        lv_obj_set_style_outline_width(hello_label, 1, 0);
-        //lv_obj_set_style_outline_color(hello_label, lv_palette_main(), 0);
-        //lv_task_handler();
-        printk("Entering main loop\n");
-
-        //lv_obj_t *count_label = lv_label_create(lv_scr_act());
-        //lv_obj_align(count_label, LV_ALIGN_CENTER, 0, 10);
-        int count = 0;
-        char count_str[16] = "1234567890abcde";
-        printk("Entering main loop %d %s\n", count, count_str);
-        //printk("Mem used before lv_task_handler: %d\n", lv_mem_get_used_size());
-
-        k_sleep(K_SECONDS(4));
-        
-        printk("After lv_task_handler\n");
-
         while(1)
         {
-                
-                lv_task_handler();
                 k_sleep(K_MSEC(50));
         }
         return 0;
