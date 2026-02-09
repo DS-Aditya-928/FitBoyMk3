b3cfb5f0702662bbf87b67cce68f9b1740fe98f9
Aditya.D.S <aditya.devendran.s@gmail.com>
2026-02-09 04:44:45 -0500

    basic notification UI

--- lib/notifications/notificationViewer.c
+++ lib/notifications/notificationViewer.c
@@ -2,7 +2,6 @@
 
 #include <notificationViewer.h>
 
-
 //d2fa52f9-4c5d-4a05-a010-c26a1b99f5e6
 #define NOTIFICATION_SERVICE_UUID_VAL BT_UUID_128_ENCODE(0xd2fa52f9, 0x4c5d, 0x4a05, 0xa010, 0xc26a1b99f5e6)
 static struct bt_uuid_128 notification_service_uuid = BT_UUID_INIT_128(NOTIFICATION_SERVICE_UUID_VAL);
@@ -40,7 +39,7 @@ static ssize_t notificationSet(struct bt_conn *conn, const struct bt_gatt_attr *
         preProcTextLen = *(int16_t*)buf;
         incomingNotification = true;
         preProcTextIndex += len - 2;
-        memcpy(preProcText, buf + 2, len - 2);
+        memcpy(preProcText, (char*)buf + 2, len - 2);
     }
 
     else
@@ -59,11 +58,6 @@ static ssize_t notificationSet(struct bt_conn *conn, const struct bt_gatt_attr *
         incomingNotification = false;
     }
 
-    //memcpy(notificationText, buf, len);
-    //notificationText[len] = 0; 
-    printk("Received notification length: %d\n", *(int16_t*)buf);
-    printk("Received notification length: %d\n", len);
-    printk("Received notification: %s\n", (char*)buf);
     return len;
 }
 
@@ -78,24 +72,65 @@ BT_GATT_SERVICE_DEFINE(notification_service,
 
 LV_FONT_DECLARE(Oswald);
 
-static lv_obj_t* screen1;
+static lv_obj_t* screen;
 static lv_group_t* g;
+static lv_style_t tight;
+
+static lv_obj_t* addNotification(lv_obj_t * parent, const char * title, const char * msg) {
+    lv_obj_t* card = lv_obj_create(parent);
+    lv_obj_add_style(card, &tight, LV_STATE_DEFAULT);
+    lv_obj_set_width(card, lv_pct(100));
+    lv_obj_set_height(card, LV_SIZE_CONTENT);
+    lv_obj_set_flex_flow(card, LV_FLEX_FLOW_ROW);
+    lv_obj_add_flag(card, LV_OBJ_FLAG_CLICKABLE);
+    lv_obj_add_flag(card, LV_OBJ_FLAG_SCROLL_ON_FOCUS);
+
+    lv_obj_t* text_cont = lv_obj_create(card);
+    lv_obj_add_style(text_cont, &tight, LV_STATE_DEFAULT);
+    lv_obj_set_flex_flow(text_cont, LV_FLEX_FLOW_COLUMN);
+    
+    lv_obj_t* lbl_title = lv_label_create(text_cont);
+    lv_obj_add_style(lbl_title, &tight, LV_STATE_DEFAULT);
+    lv_label_set_text(lbl_title, title);
+    lv_obj_set_style_text_font(lbl_title, &Oswald, 0);
+
+    lv_obj_t* lbl_msg = lv_label_create(text_cont);
+    lv_obj_add_style(lbl_msg, &tight, LV_STATE_DEFAULT);
+    lv_obj_set_width(lbl_msg, lv_pct(100)); 
+    lv_obj_set_height(lbl_msg, 32);
+    lv_label_set_long_mode(lbl_msg, LV_LABEL_LONG_WRAP);
+    lv_label_set_text(lbl_msg, msg);
+    //lv_label_set_long_mode(lbl_msg, LV_LABEL_LONG_SCROLL_CIRCULAR);
+
+    return card;
+}
 
 void NotificationViewer(void)
 {
-    screen1 = lv_obj_create(0);
+    screen = lv_obj_create(0);
     g = lv_group_create();
     k_thread_suspend(k_current_get());
     printk("Notification Viewer Loaded\n");
     
-    lv_obj_t* list = lv_list_create(screen1);
+    /*
+    lv_obj_t* list = lv_list_create(screen);
     lv_obj_set_size(list, 114, 64);
-    lv_obj_set_pos(list, 14, 0);
+    lv_obj_set_pos(list, 0, 14);
     lv_obj_set_style_pad_all(list, 0, 0);
     lv_obj_set_style_pad_row(list, 0, 0);
     lv_obj_set_style_border_width(list, 0, 0);
+    */
     
-    static lv_style_t tight;
+    
+    lv_obj_t * list = lv_obj_create(screen);
+    lv_obj_set_size(list, 128, 52);
+    lv_obj_set_pos(list, 0, 14);
+    lv_obj_set_flex_flow(list, LV_FLEX_FLOW_COLUMN);
+    lv_obj_set_style_pad_all(list, 0, 0);
+    lv_obj_set_style_pad_row(list, 0, 0);
+    lv_obj_set_style_border_width(list, 0, 0);
+    lv_obj_set_scroll_snap_y(list, LV_SCROLL_SNAP_START);
+    
     lv_style_init(&tight);
     lv_style_set_pad_ver(&tight, 1);
     lv_style_set_pad_gap(&tight, 5);
@@ -111,13 +146,11 @@ void NotificationViewer(void)
     lv_style_set_bg_opa(&tight, LV_OPA_COVER);
     lv_style_set_text_font(&tight, &Oswald);
 
-
+    /*
     static lv_style_t tightSel;
     lv_style_init(&tightSel);
     lv_style_copy(&tightSel, &tight);
     lv_style_set_pad_gap(&tightSel, 15);
-    lv_style_set_shadow_width(&tightSel, 0);
-    lv_style_set_shadow_spread(&tightSel, 0);
 
     static lv_style_t tightSelClick;
     lv_style_init(&tightSelClick);
@@ -142,11 +175,14 @@ void NotificationViewer(void)
     lv_obj_add_style(power, &tightSelClick, LV_STATE_PRESSED);
     lv_obj_remove_style(power, NULL, LV_STATE_FOCUS_KEY); //just because the default style here isnt what we want.
     
-    //lv_obj_add_style(lv_obj_get_child(flash, 0), &monoIcon, 0);
-    //lv_obj_add_style(lv_obj_get_child(power, 0), &monoIcon, 0);
-
     lv_group_add_obj(g, flash);
     lv_group_add_obj(g, power);
+    */
+
+    lv_obj_t* card1 = addNotification(list, "Test Title", "This is a test notification to demonstrate the notification viewer app. It should scroll if it exceeds the width of the screen.");
+    lv_obj_t* card2 = addNotification(list, "Second Notification", "This is another notification to show multiple notifications in the viewer. The viewer should be able to handle multiple notifications and display them properly.");
+    lv_group_add_obj(g, card1);
+    lv_group_add_obj(g, card2);
 
     while(1)
     {
@@ -162,7 +198,7 @@ K_THREAD_DEFINE(notificationViewer_thread, 16384, NotificationViewer, NULL, NULL
 App notificationViewerApp = 
 {
     .threadId = notificationViewer_thread,
-    .screen = &screen1,
+    .screen = &screen,
     .inputGroup = &g,
     .name = "Notification Viewer"
 };
