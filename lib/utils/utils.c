b38acbb2be31b95f49a7292a6944719658cbcf29
Aditya.D.S <aditya.devendran.s@gmail.com>
2026-02-16 01:37:18 -0500

    added getHeapUsage

--- lib/utils/utils.c
+++ lib/utils/utils.c
@@ -48,24 +48,29 @@ char* getPart(const char* src, const char* startTag, const char* endTag)
 
 extern struct sys_heap _system_heap;
 
-void print_heap_stats(void) {
+void print_heap_stats(void) 
+{
     struct sys_memory_stats stats;
     int ret;
 
     // Retrieve stats
     ret = sys_heap_runtime_stats_get(&_system_heap, &stats);
 
-    if (ret == 0) {
+    if (ret == 0) 
+    {
         printk("Heap Statistics:\n");
         printk("  Free:      %zu bytes\n", stats.free_bytes);
         printk("  Allocated: %zu bytes\n", stats.allocated_bytes);
         printk("  Max Usage: %zu bytes\n", stats.max_allocated_bytes);
-    } else {
+    } 
+    else 
+    {
         printk("Failed to get heap statistics (%d)\n", ret);
     }
 }
 
-void print_lvgl_heap_usage(void) {
+void print_lvgl_heap_usage(void) 
+{
     lv_mem_monitor_t mon;
     lv_mem_monitor(&mon);
 
@@ -75,4 +80,13 @@ void print_lvgl_heap_usage(void) {
     printf("  Used:  %d%%\n", mon.used_pct);
     printf("  Frag:  %d%%\n", mon.frag_pct);
     printf("  Max Used: %d bytes\n", mon.max_used);
+}
+
+size_t getHeapUsage(void)
+{
+    struct sys_memory_stats stats;
+    int ret;
+    ret = sys_heap_runtime_stats_get(&_system_heap, &stats);
+
+    return(stats.allocated_bytes);
 }
--- lib/utils/utils.h
+++ lib/utils/utils.h
@@ -1,4 +1,7 @@
+#include <stddef.h>
+
 char* getPart(const char* src, const char* startTag, const char* endTag);
 void trim(char *s);
+size_t getHeapUsage(void);
 void print_heap_stats(void);
 void print_lvgl_heap_usage(void);
