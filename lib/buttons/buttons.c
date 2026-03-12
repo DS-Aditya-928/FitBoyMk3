fd34bc3341ad0bb0b66da9ed079f820fad4e6288
Aditya.D.S <aditya.devendran.s@gmail.com>
2026-03-12 17:09:00 -0400

    Got rid of annoying cb, fixed the key getter

--- lib/buttons/buttons.c
+++ lib/buttons/buttons.c
@@ -8,30 +8,37 @@
 #include <lvgl_encoder_input.h>
 
 #define HOLD_THRESHOLD_MS 1000
-#define NON_LVGL_BUTTON -1
+#define DEBOUNCE_TIME_MS 50
+#define KEY_MODE 1
 
 lv_indev_t* indev;
 
 struct buttonData 
 {
     const struct gpio_dt_spec spec;
-    lv_key_t lvglKey;
+    lv_key_t keyCode;
+    int64_t pressTime;
     bool isPressed;
+    bool isLVGL;
 };
-#define BUTTON_INIT(node_id, lvglLabel) \
-    { .spec = GPIO_DT_SPEC_GET(node_id, gpios), .lvglKey = lvglLabel, .isPressed = false }
+#define BUTTON_INIT_LVGL(node_id, lvglLabel) \
+    { .spec = GPIO_DT_SPEC_GET(node_id, gpios), .keyCode = lvglLabel, .pressTime = 0, .isPressed = false, .isLVGL = true }
+
+#define BUTTON_INIT_NON_LVGL(node_id, key) \
+    { .spec = GPIO_DT_SPEC_GET(node_id, gpios), .keyCode = key, .pressTime = 0, .isPressed = false, .isLVGL = false }
 
 static struct buttonData buttons[] = 
 {
-    BUTTON_INIT(DT_ALIAS(up), LV_KEY_LEFT),
-    BUTTON_INIT(DT_ALIAS(down), LV_KEY_RIGHT),
-    BUTTON_INIT(DT_ALIAS(select), LV_KEY_ENTER),
-    BUTTON_INIT(DT_ALIAS(mode), NON_LVGL_BUTTON)
+    BUTTON_INIT_LVGL(DT_ALIAS(up), LV_KEY_LEFT),
+    BUTTON_INIT_LVGL(DT_ALIAS(down), LV_KEY_RIGHT),
+    BUTTON_INIT_LVGL(DT_ALIAS(select), LV_KEY_ENTER),
+    BUTTON_INIT_NON_LVGL(DT_ALIAS(mode), KEY_MODE)
 };
 
 static bool keyPressed = false;
 static int lastButton = 0;
 static int64_t modeChangeDT = 0;
+static uint16_t lvglButtonState = 0;
 
 void modeChange(struct k_work* work);
 static struct modeChangeData
@@ -63,21 +70,35 @@ void button_pressed(const struct device* dev, struct gpio_callback* cb, uint32_t
         }
     }
 
-    if(i != 3)
-    {
-        lastButton = i;
-    }
+    bool isCurrentlyPressed = gpio_pin_get_dt(&buttons[i].spec) == 1;
 
-    else
+    //rising edge
+    if(!buttons[i].isPressed && isCurrentlyPressed) 
     {
-        //rising edge
-        if(!buttons[i].isPressed && gpio_pin_get_dt(&buttons[i].spec) == 1) 
+        if(k_uptime_get() - buttons[i].pressTime < DEBOUNCE_TIME_MS)
         {
-            modeChangeDT = k_uptime_get();
+            buttons[i].pressTime = k_uptime_get();
+            return; //ignore any rising edges that occur within the debounce time
+        }
+        
+        if(buttons[i].isLVGL)
+        {
+            lastButton = i;
         }
 
-        //falling edge
-        else if(buttons[i].isPressed && gpio_pin_get_dt(&buttons[i].spec) == 0) 
+        else
+        {
+            if(buttons[i].keyCode == KEY_MODE)
+            {
+                modeChangeDT = k_uptime_get();
+            }
+        }
+    }
+
+    //falling edge
+    else if(buttons[i].isPressed && !isCurrentlyPressed) 
+    {
+        if(buttons[i].keyCode == KEY_MODE)
         {
             if((k_uptime_get() - modeChangeDT) >= HOLD_THRESHOLD_MS)
             {
@@ -91,26 +112,30 @@ void button_pressed(const struct device* dev, struct gpio_callback* cb, uint32_t
                 k_work_submit(&modeChangeData.task);
             }
         }
+        buttons[i].pressTime = k_uptime_get();
     }
 
-    buttons[i].isPressed = (gpio_pin_get_dt(&buttons[i].spec) == 1);
+    buttons[i].isPressed = isCurrentlyPressed;
+    if(buttons[i].isLVGL)
+    {
+        if(buttons[i].isPressed)
+        {
+            lvglButtonState |= BIT(i);
+        }
+
+        else
+        {
+            lvglButtonState &= ~BIT(i);
+        }
+    }
 }
 
 static struct gpio_callback pin_cb_data;
 
 void encoderRead(lv_indev_t* drv, lv_indev_data_t* data) 
 {
-    data->key = buttons[lastButton].lvglKey;
-    for(int i = 0; i < ARRAY_SIZE(buttons) - 1; i++)
-    {
-        if(buttons[i].isPressed && buttons[i].lvglKey != NON_LVGL_BUTTON)
-        {
-            data->state = LV_INDEV_STATE_PRESSED;
-            return;
-        }
-    }
-
-    data->state = LV_INDEV_STATE_RELEASED;
+    data->key = buttons[lastButton].keyCode;
+    data->state = (lvglButtonState)?LV_INDEV_STATE_PRESSED:LV_INDEV_STATE_RELEASED;
 }
 
 struct gpio_dt_spec* onButton;
